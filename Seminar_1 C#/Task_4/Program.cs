410087b3f74ed5f666b83dfe6285d27261b842f5
Samsara1988 <119740784+Samsara1988@users.noreply.github.com>
2022-12-11 19:49:12 +0300

    Added a solution Task_4

---
+++ Seminar_1 C#/Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("введите число");
+int i= Convert.ToInt32(Console.ReadLine());
+int j = 1;
+while (j <= i)
+{
+    int result = j % 2;
+    if (result != 1)
+{
+    Console.WriteLine("четное " + j);
+    j++;
+}
+else
+{
+    j++;
+}
+}
