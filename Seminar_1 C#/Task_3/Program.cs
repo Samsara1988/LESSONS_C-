4165ffeebd67b83617d387013a069414a41ad064
Samsara1988 <119740784+Samsara1988@users.noreply.github.com>
2022-12-11 19:39:52 +0300

    Added a solution Task_3

---
+++ Seminar_1 C#/Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введи число");
+int i = Convert.ToInt32(Console.ReadLine());
+int result = i%2;
+if (result== 0)
+{
+    System.Console.WriteLine("Profit");
+}
+else
+{
+    System.Console.WriteLine("No Profit");
+}
