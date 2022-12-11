221f149964970b2ced961ae4d1502c4bc3590282
Samsara1988 <119740784+Samsara1988@users.noreply.github.com>
2022-12-11 19:10:36 +0300

    Add files via upload
    Added a solution Task_1

---
+++ Seminar_1 C#/Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число m");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+{
+    Console.WriteLine("максимальное число:"+n);
+    Console.WriteLine("минимальное число:"+m);
+}
+else
+{
+    Console.WriteLine("максимальное число:"+m);
+    Console.WriteLine("минимальное число:"+n);
+}
